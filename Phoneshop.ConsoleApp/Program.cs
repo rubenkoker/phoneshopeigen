7b55263323f8d655660a070b5bc6737e1b4077aa
Ruben Koker <400034034@st.roc.a12.nl>
2022-07-28 11:38:59 +0200

    line breaks

--- Phoneshop.ConsoleApp/Program.cs
+++ Phoneshop.ConsoleApp/Program.cs
@@ -1,25 +1,25 @@
 ﻿Console.WriteLine("type productnummer\n");
-// initialisation of hte phone object
-List<Phone> List = new()
+// initialisation of the phone object
+List<Phone> list = new()
 {
 
-new Phone() {Id = 1, Brand ="Huawei",Type = "p30",Description="6.47\" FHD+ (2340x1080) OLED,"+
-" Kirin 980 Octa-Core(2x Cortex-A76 2.6GHz + 2x Cortex-A76 1.92GHz +4x Cortex-A55 1.8GHz),"+
-" 8GB RAM, 128GB ROM,40+20+8+TOF/32MP, Dual SIM, 4200mAh, Android9.0 + EMUI 9.1", Price = 697 },
-new Phone() { Id = 2,Brand = "Samsung", Type = "Galaxy A52",Description = "64 megapixel camera,"+
-" 4k videokwaliteit 6.5 inchAMOLED scherm 128 GB opslaggeheugen(Uitbreidbaar met Micro-sd)"+
-" Water- en stofbestendig(IP67", Price = 399 },
-new Phone() { Id = 3,Brand ="Apple",Type = "IPhone 11",
-Description= "Met de dubbele camera schiet je in elke situatie eenperfecte foto of video "+
-"De krachtige A13-chipset zorgtvoor razendsnelle prestaties "+
-"Met Face ID hoef je enkelen alleen naar je toestel te kijken om te ontgrendelen"+
-"Het toestel heeft een lange accuduur dankzij eenenergiezuinige processor", Price = 619 },
-new Phone() {Id = 4, Brand = "Google", Type = "Pixel 4a",
-Description= "12.2 megapixel camera, 4k videokwaliteit 5.81 inchOLED scherm "+
-"128 GB opslaggeheugen 3140 mAhaccucapaciteit", Price = 411 },
-new Phone() {Id = 5, Brand ="Xiaomi",Type = "Redmi Note 10 Pro",
-  Description = "108 megapixel camera, 4k videokwaliteit 6.67 inchAMOLED scherm "+
-  "128 GB opslaggeheugen(Uitbreidbaar met Micro-sd) Water- en stofbestendig(IP53)", Price = 298 },
+    new Phone() {Id = 1, Brand ="Huawei",Type = "p30",Description="6.47\" FHD+ (2340x1080) OLED,"+
+    " Kirin 980 Octa-Core(2x Cortex-A76 2.6GHz + 2x Cortex-A76 1.92GHz +4x Cortex-A55 1.8GHz),"+
+    " 8GB RAM, 128GB ROM,40+20+8+TOF/32MP, Dual SIM, 4200mAh, Android9.0 + EMUI 9.1", Price = 697 },
+    new Phone() { Id = 2,Brand = "Samsung", Type = "Galaxy A52",Description = "64 megapixel camera,"+
+    " 4k videokwaliteit 6.5 inchAMOLED scherm 128 GB opslaggeheugen(Uitbreidbaar met Micro-sd)"+
+    " Water- en stofbestendig(IP67", Price = 399 },
+    new Phone() { Id = 3,Brand ="Apple",Type = "IPhone 11",
+    Description= "Met de dubbele camera schiet je in elke situatie eenperfecte foto of video "+
+    "De krachtige A13-chipset zorgtvoor razendsnelle prestaties "+
+    "Met Face ID hoef je enkelen alleen naar je toestel te kijken om te ontgrendelen"+
+    "Het toestel heeft een lange accuduur dankzij eenenergiezuinige processor", Price = 619 },
+    new Phone() {Id = 4, Brand = "Google", Type = "Pixel 4a",
+    Description= "12.2 megapixel camera, 4k videokwaliteit 5.81 inchOLED scherm "+
+    "128 GB opslaggeheugen 3140 mAhaccucapaciteit", Price = 411 },
+    new Phone() {Id = 5, Brand ="Xiaomi",Type = "Redmi Note 10 Pro",
+      Description = "108 megapixel camera, 4k videokwaliteit 6.67 inchAMOLED scherm "+
+      "128 GB opslaggeheugen(Uitbreidbaar met Micro-sd) Water- en stofbestendig(IP53)", Price = 298 },
 };
 
 ConsoleKeyInfo input = Console.ReadKey();
@@ -30,19 +30,19 @@ Console.Clear();
 switch (inputValue)
 {
     case "1":
-        Console.WriteLine($"{List[0].Brand}" + " " + $"{List[0].Type}" + " " + $"{List[0].Description}" + " prijs =" + $"{List[0].Price}");
+        Console.WriteLine($"{list[0].Brand} \n{list[0].Type}\n {list[0].Description}\n  prijs ={list[0].Price}");
         break;
     case "2":
-        Console.WriteLine($"{List[1].Brand}" + " " + $"{List[1].Type}" + " " + $"{List[1].Description}" + " prijs =" + $"{List[1].Price}");
+        Console.WriteLine($"{list[1].Brand}\n{list[1].Type}\n{list[1].Description}\n prijs ={list[1].Price}");
         break;
     case "3":
-        Console.WriteLine($"{List[2].Brand}" + " " + $"{List[2].Type}" + " " + $"{List[2].Description}" + " prijs =" + $"{List[2].Price}");
+        Console.WriteLine($"{list[2].Brand}\n{list[2].Type}\n{list[2].Description}\n prijs ={list[2].Price}");
         break;
     case "4":
-        Console.WriteLine($"{List[3].Brand}" + " " + $"{List[3].Type}" + " " + $"{List[3].Description}" + " prijs =" + $"{List[3].Price}");
+        Console.WriteLine($"{list[3].Brand}\n{list[3].Type}\n{list[3].Description}\n prijs ={list[3].Price}");
         break;
     case "5":
-        Console.WriteLine($"{List[4].Brand}" + " " + $"{List[4].Type}" + " " + $"{List[4].Description}" + " prijs =" + $"{List[4].Price}");
+        Console.WriteLine($"{list[4].Brand}\n{list[4].Type}\n{list[4].Description}\n prijs ={list[4].Price}");
         break;
     default:
         Console.WriteLine("geen geldige knop");
