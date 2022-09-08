34deec9635cfb065ce84de8c16ddaa706d97a477
Ruben Koker <400034034@st.roc.a12.nl>
2022-09-08 11:00:03 +0200

    phoneservice naar SQL omgezet

--- Phoneshop.Business/PhoneService.cs
+++ Phoneshop.Business/PhoneService.cs
@@ -7,65 +7,6 @@ namespace Phoneshop.Business;
 public class PhoneService : IPhoneService
 {
 
-    private readonly List<Phone> _phones = new()
-    {
-
-        new Phone()
-        {
-            Id = 1,
-            Brand = "Huawei",
-            Type = "P30",
-            Description = "6.47\" FHD+ (2340x1080) OLED," +
-        " Kirin 980 Octo-Core (2x Cortex-A76 2.6GHz + 2x Cortex-A76 1.92GHz +4x Cortex-A55 1.8GHz)," +
-        " 8GB RAM, 128GB ROM, 40+20+8+TOF/32MP, Dual SIM, 4200mAh, Android9.0 + EMUI 9.1",
-            Price = 697,
-            Stock = 32
-        },
-        new Phone()
-        {
-            Id = 2,
-            Brand = "Samsung",
-            Type = "Galaxy A52",
-            Description = "64 megapixel camera," +
-        " 4k videokwaliteit 6.5 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd)" +
-        " Water- en stofbestendig(IP67)",
-            Price = 399,
-            Stock = 42
-        },
-        new Phone()
-        {
-            Id = 3,
-            Brand = "Apple",
-            Type = "IPhone 11",
-            Description = "Met de dubbele camera schiet je in elke situatie een perfecte foto of video " +
-        "De krachtige A13-chipset zorgt voor razendsnelle prestaties " +
-        "Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen " +
-        "Het toestel heeft een lange accuduur dankzij een energiezuinige processor",
-            Price = 619,
-            Stock = 54
-        },
-        new Phone()
-        {
-            Id = 4,
-            Brand = "Google",
-            Type = "Pixel 4a",
-            Description = "12.2 megapixel camera, 4k videokwaliteit 5.81 inch OLED scherm " +
-        "128 GB opslaggeheugen 3140 mAh accucapaciteit",
-            Price = 411,
-            Stock = 58
-        },
-        new Phone()
-        {
-            Id = 5,
-            Brand = "Xiaomi",
-            Type = "Redmi Note 10 Pro",
-            Description = "108 megapixel camera, 4k videokwaliteit 6.67 inch AMOLED scherm " +
-          "128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig(IP53)",
-            Price = 298,
-            Stock = 91
-        },
-    };
-
     /// <summary>
     /// returns the phone corresponding to the ID
     /// </summary>
@@ -73,8 +14,38 @@ public class PhoneService : IPhoneService
     /// <returns></returns>
     public Phone? GetPhoneById(int input)
     {
+        Phone _result = new();
+        string queryString = @$"SELECT * FROM Phones
+                                WHERE Id LIKE '%{input}%' ; ";
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=phoneshop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
 
-        return _phones.Where(s => s.Id == input).FirstOrDefault();
+        using (SqlConnection connection = new SqlConnection(
+                   connectionString))
+        {
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Call Read before accessing data.
+
+            _result = (new Phone()
+            {
+                Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                Brand = reader.GetString(reader.GetOrdinal("Brand")),
+                Type = reader.GetString(reader.GetOrdinal("Type")),
+                Description = reader.GetString(reader.GetOrdinal("Description")),
+                Price = (Decimal)reader.GetSqlDecimal(reader.GetOrdinal("Price")),
+                Stock = reader.GetInt32(reader.GetOrdinal("Stock")),
+
+            });
+
+            // Call Close when done reading.
+            reader.Close();
+        }
+
+        return _result;
+
     }
 
     /// <summary>
@@ -124,16 +95,39 @@ public class PhoneService : IPhoneService
     /// <returns></returns>
     public List<Phone>? SearchPhonesByString(string input)
     {
-        if (input == "")
+        List<Phone> _result = new();
+        string queryString = @$"SELECT * FROM Phones
+                                WHERE Brand LIKE '%{input}%' OR Type LIKE '%{input}%' OR Description LIKE '%{input}%' ; ";
+        string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=phoneshop;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        using (SqlConnection connection = new SqlConnection(
+                   connectionString))
         {
-            return null;
+            SqlCommand command = new SqlCommand(queryString, connection);
+            command.Connection.Open();
+            command.ExecuteNonQuery();
+            SqlDataReader reader = command.ExecuteReader();
+
+            // Call Read before accessing data.
+            while (reader.Read())
+            {
+                _result.Add(new Phone()
+                {
+                    Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                    Brand = reader.GetString(reader.GetOrdinal("Brand")),
+                    Type = reader.GetString(reader.GetOrdinal("Type")),
+                    Description = reader.GetString(reader.GetOrdinal("Description")),
+                    Price = (Decimal)reader.GetSqlDecimal(reader.GetOrdinal("Price")),
+                    Stock = reader.GetInt32(reader.GetOrdinal("Stock")),
+
+                });
+            }
+
+            // Call Close when done reading.
+            reader.Close();
         }
 
-        return (from t in _phones
-                where
-                t.Brand.Contains(input, StringComparison.CurrentCultureIgnoreCase) ||
-                t.Description.Contains(input, StringComparison.CurrentCultureIgnoreCase) ||
-                t.Type.Contains(input, StringComparison.CurrentCultureIgnoreCase)
-                select t).ToList();
+        return _result;
+
     }
 }
--- SQLQuery2.sql
+++ SQLQuery2.sql
@@ -8,6 +8,7 @@ CREATE TABLE [dbo].[Phones]
     [Price] DECIMAL NULL,
     [Stock ] INT NULL
 )
+Delete  from phones;
 CREATE TABLE [dbo].[Phones]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
@@ -18,11 +19,7 @@ CREATE TABLE [dbo].[Phones]
     [Stock ] INT NULL
 )
 INSERT INTO phones([Brand], [Type], [Description], [Price],[Stock])
-VALUES ('Samsung', 'Galaxy A52', '64 megapixel camera,+
-        " 4k videokwaliteit 6.5 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd)"+
-        " Water- en stofbestendig(IP67)',  399,42
-       )
-,
+VALUES 
 
  ('Samsung', 'Galaxy A52', '64 megapixel camera,'+
         ' 4k videokwaliteit 6.5 inch AMOLED scherm 128 GB opslaggeheugen (Uitbreidbaar met Micro-sd)'+
@@ -48,24 +45,9 @@ VALUES ('Samsung', 'Galaxy A52', '64 megapixel camera,+
      )
      ,
  ( 'Huawei', 'P30','6.47" FHD+ (2340x1080) OLED, Kirin 980 Octa-Core (2x Cortex-A76 2.6GHz + 2x Cortex-A76 1.92GHz + 4x Cortex-A55 1.8GHz), 8GB RAM, 128GB ROM, 40+20+8+TOF/32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1',  697 ,32
-       ),(
- 'Apple','IPhone 11',
-         'Met de dubbele camera schiet je in elke situatie een perfecte foto of video '+
-        'De krachtige A13-chipset zorgt voor razendsnelle prestaties '+
-        'Met Face ID hoef je enkel en alleen naar je toestel te kijken om te ontgrendelen '+
-       ' Het toestel heeft een lange accuduur dankzij een energiezuinige processor', 619,54 
-     )
-        , (
-          'Google',  'Pixel 4a',
-         '12.2 megapixel camera, 4k videokwaliteit 5.81 inch OLED scherm '+
-        '128 GB opslaggeheugen 3140 mAh accucapaciteit',  411,58
-
-     ), (
-          'Xiaomi', 'Redmi Note 10 Pro',
-           '108 megapixel camera, 4k videokwaliteit 6.67 inch AMOLED scherm'+
-          '128 GB opslaggeheugen (Uitbreidbaar met Micro-sd) Water- en stofbestendig(IP53)',  298,91
-
-     );
-     INSERT INTO phones( Brand, Type, Description, Price,Stock)
-VALUES ( 'Huawei', 'P30','6.47" FHD+ (2340x1080) OLED, Kirin 980 Octa-Core (2x Cortex-A76 2.6GHz + 2x Cortex-A76 1.92GHz + 4x Cortex-A55 1.8GHz), 8GB RAM, 128GB ROM, 40+20+8+TOF/32MP, Dual SIM, 4200mAh, Android 9.0 + EMUI 9.1',  697 ,32
        );
+  
+   
+   ALTER TABLE phones
+ADD [Stock2] int; 
+UPDATE Phones SET [Stock2] = [Stock ];
