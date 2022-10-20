29733c2ffcf48751a61630c7d19780002139b1b1
Ruben Koker <400034034@st.roc.a12.nl>
2022-10-20 14:39:29 +0200

    database operations service interface

---
+++ Phoneshop.Domain/Interfaces/IPhoneRepository.cs
@@ -0,0 +1,14 @@
+﻿using Phoneshop.Domain.Models;
+
+namespace Phoneshop.Domain.Interfaces
+{
+    public interface IPhoneRepository
+    {
+        public void Create(Phone input);
+        public void Update(Phone input);
+        public void Delete(Phone input);
+        public List<Phone> GetAll();
+        public Phone Get(int id);
+
+    }
+}
